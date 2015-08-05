44fda55d4891d775f5b9d438695a0f30ddd2947f
Dylan Ball <Arathnim@gmail.com>
2015-08-04 23:26:14 -0500

    Completed command dispatch system

--- init-test.cl
+++ init-test.cl
@@ -4,6 +4,13 @@
 (declaim #+sbcl(sb-ext:muffle-conditions warning))
 (setq *print-pretty* 'nil)
 (proclaim '(optimize (speed 0) (safety 3) (debug 3) (space 0)))
+;; reader macros
+(defun single-quote-reader (stream char)
+   (declare (ignore char))
+   `(gethash ',(read stream t nil t) matched-symbols))
+
+(set-macro-character #\% #'single-quote-reader)
+;; end reader macros
 (asdf:load-system :mushy)
 (sb-thread:make-thread 'ticker :name "ticker")
 (sb-thread:make-thread 'start-server :name "server")
--- mushy-control.lisp
+++ mushy-control.lisp
@@ -1,9 +1,81 @@
+(defparameter *commands* nil)
+
+(defun defcommand (head &rest forms)
+	(push (list head forms) *commands*))
+
+;; WARNING! Psychotic code - approach with caution
+
+(defun mushy-eval2 (command-str caller)
+	(setf command-str (string-trim '(#\Space #\Tab #\Newline) command-str))
+	(let* ((command (find-head command-str)) 
+			 (forms (cadr command))
+			 (form nil)
+			 (matches nil)
+			 (symbols nil)
+			 (matched-symbols (make-hash-table :test 'eq)))
+		(declare (special matched-symbols))
+		(block outer 
+			(loop for f in forms do
+				(multiple-value-bind (a b) 
+					(cl-ppcre:scan-to-strings (convert-form f) command-str)
+						(if a (progn 
+							(setf matches b)
+							(setf form f)
+							(setf symbols (collect-symbols f))
+							(return-from outer nil))))))
+		(if (and symbols (not (equalp matches #()))) 
+			(loop for s in symbols for m across matches do
+			(setf (gethash s matched-symbols) m)))
+		(let ((player caller))
+			(declare (special player))
+			(eval (cadr form)))))
+
+(defun convert-head (head)
+	(format nil "^~a" head))
+
+(defun convert-optional (str)
+	(format nil "(?:~a)?" str))
+
+(defun convert-switch (list)
+	(format nil "(?:~{~a~^|~})" list))
+
+(defun cat-list (list)
+	(format nil "~{~a~}" list))
+
+(defun find-head (command)
+	(loop for c in *commands* do
+		(if (cl-ppcre:all-matches (convert-head (car c)) command)
+			(return-from find-head c))))
+
+(defun convert-form (form)
+	(setf form (car form))
+	(let ((res "^"))
+		(loop for s in form do
+			(cond ((stringp s) (setf res (catstr res s)))
+					((symbolp s) (setf res (catstr res "(.+?)")))
+					((eq (car s) 'optional) (setf res
+						(catstr res (cat-list (mapcar #'convert-optional (cdr s))))))
+					((eq (car s) 'switch) (setf res
+						(catstr res (convert-switch (cdr s))))))) 
+		(format nil "~a$" res)))
+
+(defun collect-symbols (form)
+	(remove nil (mapcar (lambda (x) (if (symbolp x) x)) (car form))))
+
+(defmacro get- (sym) `(gethash ',sym matched-symbols))
+
+(defcommand "test"
+	'(("test value2 " val1 " ~ " val2) (list (get- val1) (get- val2)))
+	'(("test value " val) (list "Ohh Rah!" %val))
+	'(("test test") "WOOOO!")
+	'(("test") "Testing, testing, 1, 2, 3..."))
+
 (defun ex (blk) 
 	(format nil "Attributes:~%~{~a~} ~%Flags:~{~a ~}~%Above: ~a~%Subs:~%~{~a~%~}"
-			 (loop for key being each hash-key of (attrs blk)
-			 	collect (print-hash-entry key (gethash key (attrs blk))))
-			 (flags blk) 
-			 (if (above blk) (attr (above blk) "name") nil) (get-sub-names blk)))
+		(loop for key being each hash-key of (attrs blk)
+			collect (print-hash-entry key (gethash key (attrs blk))))
+		(flags blk) (if (above blk) (attr (above blk) "name") nil) 
+		(get-sub-names blk)))
 
 (defun print-hash-entry (key value)
     (format nil "~a : ~a~%" key value))
@@ -15,7 +87,7 @@
 				"Can't see that here."))
 		(exec-attr (above player) "desc" player nil)))
 
-(defun extern-quit (args player) 
+(defun extern-quit (args player)
   (let ((socket (get-socket (attr player "name"))))
 		;;(remove (list (attr player "name") socket) *sockets*)
   		(usocket:socket-close socket)))
@@ -43,8 +115,8 @@
 	 (if (and target container) (put-into container target) "Can't see that here.")))
 
 (defun extern-say (args player)
-	(broadcast (format nil "[~a] ~a" (attr player "name") (reconstruct args)) (above player))
-	"")
+	(broadcast (format nil "[~a] ~a" (attr player "name") 
+		(reconstruct args)) (above player)) "")
 
 (defun extern-set (args player)
   (let ((target (find-sub (above player) (car args))))
@@ -57,8 +129,6 @@
 
 (defun reconstruct (arg)
 	(format nil "~{~a~^ ~}" arg))
-
-(defparameter *commands* '("look" "quit" "ex" "save" "set" "say" "place" "go"))
 
 (defun mushy-eval (str caller) 
 	(print `(,caller ,str))
@@ -74,12 +144,3 @@
 	(if (equalp str "here") (return-from resolve-object-descriptor (above player)))
 	(if (equalp str "me") (return-from resolve-object-descriptor player))
 	(find-sub (above player) str))
-
-;; (obj remaining-args)
-#| (defun consume-object-descriptor (args player)
-	(let ((result nil))
-		(if (car args) 
-			(cond ((equalp (car args) "here") (setf result (list (above player) (cadr args)))
-					((equalp (car args) "me") (setf result player))
-					)))) |#
-
